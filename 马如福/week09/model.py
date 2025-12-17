cdfe83d0bf033c8af008cb678d408f191b37314a
rufuma <58943585+rufuma@users.noreply.github.com>
2025-12-17 21:51:52 +0800

    Create model.py

---
+++ 马如福/week09/model.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+import torch
+import torch.nn as nn
+from torch.optim import Adam, SGD
+from torchcrf import CRF
+from transformers import BertModel
+"""
+建立网络模型结构
+"""
+
+class TorchModel(nn.Module):
+    def __init__(self, config):
+        super(TorchModel, self).__init__()
+        #hidden_size = config["hidden_size"]
+        #vocab_size = config["vocab_size"] + 1
+        #max_length = config["max_length"]
+        class_num = config["class_num"]
+        #num_layers = config["num_layers"]
+        #self.embedding = nn.Embedding(vocab_size, hidden_size, padding_idx=0)
+        #self.layer = nn.LSTM(hidden_size, hidden_size, batch_first=True, bidirectional=True, num_layers=num_layers)
+        self.encoder = BertModel.from_pretrained(config["bert_path"], return_dict=False)
+        hidden_size = self.encoder.config.hidden_size
+        self.classify = nn.Linear(hidden_size, class_num)
+        self.crf_layer = CRF(class_num, batch_first=True)
+        self.use_crf = config["use_crf"]
+        self.loss = torch.nn.CrossEntropyLoss(ignore_index=config["ignore_index"])  #loss采用交叉熵损失
+
+    #当输入真实标签，返回loss值；无真实标签，返回预测值
+    def forward(self, input_ids, attention_mask, target=None):
+        #x = self.embedding(x)  #input shape:(batch_size, sen_len)
+        #x, _ = self.layer(x)      #input shape:(batch_size, sen_len, hidden_size * 2)
+        x = self.encoder(input_ids, attention_mask=attention_mask)
+        if isinstance(x, tuple):
+            x = x[0]
+        predict = self.classify(x) #ouput:(batch_size, sen_len, num_tags) -> (batch_size * sen_len, num_tags)
+
+        if target is not None:
+            if self.use_crf:
+                crf_tags = target.clone()
+                crf_tags[crf_tags == self.loss.ignore_index] = 8
+                mask = (target != self.loss.ignore_index).bool()
+                mask[:, 0] = True
+                #aimport pdb;pdb.set_trace()
+                return - self.crf_layer(predict, crf_tags, mask=mask, reduction="mean")
+            else:
+                #(number, class_num), (number)
+                return self.loss(predict.view(-1, predict.shape[-1]), target.view(-1))
+        else:
+            if self.use_crf:
+                mask = attention_mask.bool()
+                mask[:, 0] = True
+                return self.crf_layer.decode(predict, mask=mask)
+            else:
+                return predict
+
+
+def choose_optimizer(config, model):
+    optimizer = config["optimizer"]
+    learning_rate = config["learning_rate"]
+    if optimizer == "adam":
+        return Adam(model.parameters(), lr=learning_rate)
+    elif optimizer == "sgd":
+        return SGD(model.parameters(), lr=learning_rate)
+
+
+if __name__ == "__main__":
+    from config import Config
+    model = TorchModel(Config)
