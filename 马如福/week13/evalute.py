2aea6e483e03692d20443d806fcb8b2c2d420612
rufuma <58943585+rufuma@users.noreply.github.com>
2026-01-21 22:25:24 +0800

    Create evalute.py

---
+++ 马如福/week13/evalute.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+import torch
+from loader import load_data
+
+"""
+模型效果测试
+"""
+
+class Evaluator:
+    def __init__(self, config, model, logger):
+        self.config = config
+        self.model = model
+        self.logger = logger
+        self.valid_data = load_data(config["valid_data_path"], config, shuffle=False)
+        self.stats_dict = {"correct":0, "wrong":0}  #用于存储测试结果
+
+    def eval(self, epoch):
+        self.logger.info("开始测试第%d轮模型效果：" % epoch)
+        self.model.eval()
+        self.stats_dict = {"correct": 0, "wrong": 0}  # 清空上一轮结果
+        for index, batch_data in enumerate(self.valid_data):
+            if torch.cuda.is_available():
+                batch_data = [d.cuda() for d in batch_data]
+            input_ids, labels = batch_data   #输入变化时这里需要修改，比如多输入，多输出的情况
+            with torch.no_grad():
+                pred_results = self.model(input_ids)[0]
+            self.write_stats(labels, pred_results)
+        acc = self.show_stats()
+        return acc
+
+    def write_stats(self, labels, pred_results):
+        # assert len(labels) == len(pred_results)
+        pred_labels = torch.argmax(pred_results, dim=-1)
+        for true_seq, pred_seq in zip(labels, pred_labels):
+            for true_label, pred_label in zip(true_seq, pred_seq):
+                self.stats_dict["total"] += 1
+                if int(true_label) == int(pred_label) and int(true_label) != 0:  # 可选：忽略O标签的准确率
+                    self.stats_dict["correct"] += 1
+        return
+
+    def show_stats(self):
+        correct = self.stats_dict["correct"]
+        wrong = self.stats_dict["wrong"]
+        self.logger.info("预测集合条目总量：%d" % (correct +wrong))
+        self.logger.info("预测正确条目：%d，预测错误条目：%d" % (correct, wrong))
+        self.logger.info("预测准确率：%f" % (correct / (correct + wrong)))
+        self.logger.info("--------------------")
+        return correct / (correct + wrong)
